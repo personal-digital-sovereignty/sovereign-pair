7a5dc56cd563d099ab25ede0a6f2a3d82095f3b3
Jeferson Lopes <jefersonlopes.br@gmail.com>
2025-10-05 18:22:55 +0000

    feat: ingest.py concluso. Criado agent.py, que será como o Ollama decidirá se os arquivos locais são suficientes ou se utilizará do motor do DuckDuckGo em busca de uma ou outra decisão para resolver dado problema.

--- src/ingest.py
+++ src/ingest.py
@@ -2,3 +2,27 @@ import chromadb
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from config import embed_model
+
+def ingest_data():
+    print("Carregando documentos...")
+    # O SimpleDirectoryReader lê nativamente .md, .pdf, .docx, .csv
+    documents = SimpleDirectoryReader("../data/raw_docs", recursive=True).load_data()
+    documents += SimpleDirectoryReader("../data/vault", recursive=True).load_data()
+
+    print("Inicializando ChromaDB...")
+    db = chromadb.PersistentClient(path="../data/chromadb")
+    chroma_collection = db.get_or_create_collection("sovereign_knowledge")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    print("Gerando embeddings e indexando (isso pode levar um tempo na primeira vez)...")
+    index = VectorStoreIndex.from_documents(
+        documents, 
+        storage_context=storage_context, 
+        embed_model=embed_model
+    )
+    print("Indexação concluída!")
+
+if __name__ == "__main__":
+    ingest_data()
