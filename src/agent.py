d0cfddd4a217b272566c8b44780efe024e498c74
Jeferson Lopes <jefersonlopes.br@gmail.com>
2025-10-05 22:16:50 +0000

    feat: Conf para nossa tool buscas em arquivos locais, realizando o index dos vetores no ChromaDb

---
+++ src/agent.py
@@ -0,0 +1,25 @@
+import chromadb
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from duckduckgo_search import DDGS
+from llama_index.core.tools import FunctionTool
+from config import llm, embed_model
+
+# 1. Configuração para o acesso local aos arquivos (RAG)
+db = chromadb.PersistentClient(path="../data/chromadb")
+chroma_collection = db.get_or_create_collection("sovereign_knowledge")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+index = VectorStoreIndex.from_vector_store(vector_store, embed_model=embed_model)
+
+local_query_engine = index.as_query_engine(llm=llm)
+
+local_tool = QueryEngineTool(
+    query_engine=local_query_engine,
+    metadata=ToolMetadata(
+        name="arquivos_pessoais",
+        description="Útil para busca de informações nos arquivos locais, PDFs, anotações em Markdown e documentações pessoais do usuário."
+    ),
+)
+
