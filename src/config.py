2f56e6fd21480b93e6a4f77fbce7af6daa9bc8ba
Jeferson Lopes <jefersonlopes.br@gmail.com>
2025-10-11 19:45:24 +0000

    chore: Atualei o modelo LLM de llama3.1 para llama3.2. Esta alteraćão para o modelo llama3.2 é buscando uma melhor performance e compatibilidade com o Agent (ReAct)

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
 
 # Configuração do LLM para futuro chat e definição de request para o prompt
-llm = Ollama(model="llama3.1", request_timeout=120.0)
+llm = Ollama(model="llama3.2", request_timeout=120.0)
 
 # Configuração do modelo que irá indexar os arquivos e transformar em vetores no chromadb
 embed_model = OllamaEmbedding(model_name="nomic-embed-text")
