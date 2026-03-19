ac94770d718c58969359b6a148b74c04b5f89039
Jeferson Lopes <jefersonlopes.br@gmail.com>
2026-03-19 13:01:05 -0300

    chore(core): Physical purge of obsolete Python backend (The Chacina)

--- tests/sandbox/test_jeferson_tree.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from src.api.database import SessionLocal
-from src.api.routes import get_authorized_workspaces
-db = SessionLocal()
-res = get_authorized_workspaces(db, "Jeferson")
-print("Folders Resolvidas (ExpandUser + Abspath):", res)
-db.close()
